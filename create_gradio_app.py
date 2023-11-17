58e6077096b9d6ec2b188797b896e11230e90b1e
isayhc <isayahculbertson@gmail.com>
2023-11-17 18:19:11 -0500

    added some relavent examples

--- create_gradio_app.py
+++ create_gradio_app.py
@@ -7,12 +7,19 @@ from langchain.llms import Cohere
 from create_chain import chain as llm_chain
 
 
+examples = [
+    ["The tower is 324 metres (1,063 ft) tall,"],
+    ["The Moon's orbit around Earth has"],
+    ["The smooth Borealis basin in the Northern Hemisphere covers 40%"],
+]
 
 def create_UI(llm_chain):
     with gr.Blocks() as demo:
         chatbot = gr.Chatbot()
         msg = gr.Textbox()
         clear = gr.Button("Clear")
+        # gr.Examples(examples=examples, label="Examples", inputs=gr.Textbox(lines=5, max_lines=6, label=chatbot),)
+        gr.Examples(examples=examples, label="Examples", inputs=msg,)
 
         def user(user_message, history):
             return "", history + [[user_message, None]]
