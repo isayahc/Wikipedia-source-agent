0efb006bc8f46873bb0b134c35783dafac9f0000
isayhc <isayahculbertson@gmail.com>
2023-11-17 16:48:41 -0500

    bring UI creator to main

---
+++ create_gradio_app.py
@@ -0,0 +1,34 @@
+import gradio as gr
+
+from langchain import LLMChain
+from langchain import PromptTemplate
+from langchain.llms import Cohere
+
+
+
+def create_UI(llm_chain):
+    with gr.Blocks() as demo:
+        chatbot = gr.Chatbot()
+        msg = gr.Textbox()
+        clear = gr.Button("Clear")
+
+        def user(user_message, history):
+            return "", history + [[user_message, None]]
+
+        def bot(history):
+            print("Question: ", history[-1][0])
+            bot_message = llm_chain(history[-1][0])
+
+            bot_message = bot_message["text"]
+            print("Response: ", bot_message)
+            history[-1][1] = ""
+            history[-1][1] += bot_message
+            return history
+
+        msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)
+        clear.click(lambda: None, None, chatbot, queue=False)
+    return demo
+
+
+if __name__ == "__main__":
+    pass
