b1fd04628012903c6c44a6e129d2ec8e64ca26d2
isayhc <isayahculbertson@gmail.com>
2023-11-17 16:38:26 -0500

    fixed errors involving gradio and chain interfacing

--- alt_app.py
+++ alt_app.py
@@ -10,46 +10,15 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env. 
 # https://pypi.org/project/python-dotenv/
 
-
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
-# Initializing the LLM and the Toolset
-llm = chat = Cohere(
-    cohere_api_key=COHERE_API_KEY,
-    )
 
-
-template = """Question: {question}
-Answer: Let's think step by step."""
-prompt = PromptTemplate(template=template, input_variables=["question"])
-
-llm_chain = LLMChain(prompt=prompt, llm=llm)
-
-with gr.Blocks() as demo:
-    chatbot = gr.Chatbot()
-    msg = gr.Textbox()
-    clear = gr.Button("Clear")
-    llm_chain, llm = llm, llm
-
-    def user(user_message, history):
-        return "", history + [[user_message, None]]
-
-    def bot(history):
-        print("Question: ", history[-1][0])
-        bot_message = llm_chain(history[-1][0])
-        print("Response: ", bot_message)
-        history[-1][1] = ""
-        history[-1][1] += bot_message
-        return history
-
-    msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)
-    clear.click(lambda: None, None, chatbot, queue=False)
 
 def create_UI(llm_chain):
     with gr.Blocks() as demo:
         chatbot = gr.Chatbot()
         msg = gr.Textbox()
         clear = gr.Button("Clear")
-        llm_chain, llm = llm, llm
+        # llm_chain, llm = llm, llm
 
         def user(user_message, history):
             return "", history + [[user_message, None]]
@@ -57,6 +26,8 @@ def create_UI(llm_chain):
         def bot(history):
             print("Question: ", history[-1][0])
             bot_message = llm_chain(history[-1][0])
+
+            bot_message = bot_message["text"]
             print("Response: ", bot_message)
             history[-1][1] = ""
             history[-1][1] += bot_message
@@ -68,5 +39,20 @@ def create_UI(llm_chain):
 
 
 if __name__ == "__main__":
+    
+    # Initializing the LLM and the Toolset
+    llm = chat = Cohere(
+        cohere_api_key=COHERE_API_KEY,
+        )
+
+
+    template = """Question: {question}
+    Answer: Let's think step by step."""
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+    demo = create_UI(llm_chain)
+
     demo.queue()
     demo.launch()
