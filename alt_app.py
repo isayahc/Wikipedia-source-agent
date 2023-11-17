0a294fdd8a99f98b7e58d09e211880081103a60e
isayhc <isayahculbertson@gmail.com>
2023-11-17 16:30:23 -0500

    made function to create UI for a chain

--- alt_app.py
+++ alt_app.py
@@ -44,6 +44,28 @@ with gr.Blocks() as demo:
     msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)
     clear.click(lambda: None, None, chatbot, queue=False)
 
+def create_UI(llm_chain):
+    with gr.Blocks() as demo:
+        chatbot = gr.Chatbot()
+        msg = gr.Textbox()
+        clear = gr.Button("Clear")
+        llm_chain, llm = llm, llm
+
+        def user(user_message, history):
+            return "", history + [[user_message, None]]
+
+        def bot(history):
+            print("Question: ", history[-1][0])
+            bot_message = llm_chain(history[-1][0])
+            print("Response: ", bot_message)
+            history[-1][1] = ""
+            history[-1][1] += bot_message
+            return history
+
+        msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)
+        clear.click(lambda: None, None, chatbot, queue=False)
+    return demo
+
 
 if __name__ == "__main__":
     demo.queue()
