cd75d762e79bd791be2e6b62b18617ede4ab334e
isayhc <isayahculbertson@gmail.com>
2023-11-17 22:00:15 -0500

    fixed typo with general_internet_template

--- prompt.py
+++ prompt.py
@@ -26,31 +26,31 @@ fact_2 [source_2, source_3]
 Answer: 
 """
 
-general_internet_template = """Question: {query}
+# general_internet_template = """Question: {query}
 
-Please only use {website_list} when searching for the answer.
-
-
-When given a query you must generate a wikipedia article based on the query given;
-You must oranization your article into sections just like in wikipedia
-The structure is open ended however you must write this article in markdown;
-Also you must have a reference section at the end with a list of all your refernces;
-If you are unsure about the exact person the user is refering to please ask questions;
-
-For the sake of clarity please add new lines between your inital output and the
-generated wikipedia article
+# Please only use {website_list} when searching for the answer.
 
 
-If there are many pages for a similar person or entity please as
-the user to specify which one they are talking about before geenrating the article
+# When given a query you must generate a wikipedia article based on the query given;
+# You must oranization your article into sections just like in wikipedia
+# The structure is open ended however you must write this article in markdown;
+# Also you must have a reference section at the end with a list of all your refernces;
+# If you are unsure about the exact person the user is refering to please ask questions;
 
-Please make sure to include in-line citations
+# For the sake of clarity please add new lines between your inital output and the
+# generated wikipedia article
 
-for example:
-fact_1 [source_1]
-fact_2 [source_2, source_3]
-Answer: 
-"""
+
+# If there are many pages for a similar person or entity please as
+# the user to specify which one they are talking about before geenrating the article
+
+# Please make sure to include in-line citations
+
+# for example:
+# fact_1 [source_1]
+# fact_2 [source_2, source_3]
+# Answer: 
+# """
 
 
 general_internet_template = """Question: {query}
