ceba0c4e5e53fab56c602ccc4ec0024fde2d45ed
isayhc <isayahculbertson@gmail.com>
2023-11-17 22:01:23 -0500

    better intergrating backend and UI

--- create_gradio_app.py
+++ create_gradio_app.py
@@ -4,7 +4,21 @@ from langchain import LLMChain
 from langchain import PromptTemplate
 from langchain.llms import Cohere
 
-from create_chain import chain as llm_chain
+# from create_chain import chain as llm_chain
+from create_chain import create_chain_from_template
+from prompt import wikipedia_template, general_internet_template
+from langchain.retrievers import CohereRagRetriever
+from langchain.chat_models import ChatCohere
+
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()  # take environment variables from .env. 
+# https://pypi.org/project/python-dotenv/
+
+COHERE_API_KEY = os.getenv("COHERE_API_KEY")
 
 
 examples = [
@@ -18,11 +32,15 @@ def create_UI(llm_chain):
         radio = gr.Radio(
         ["wikipedia only", "any website", "none"], label="What kind of essay would you like to write?"
     )
+
+        
         chatbot = gr.Chatbot()
         msg = gr.Textbox()
         clear = gr.Button("Clear")
-        # gr.Examples(examples=examples, label="Examples", inputs=gr.Textbox(lines=5, max_lines=6, label=chatbot),)
+        submit_btn = gr.Button("Submit", variant="primary")
+
         gr.Examples(examples=examples, label="Examples", inputs=msg,)
+        
 
         def user(user_message, history):
             return "", history + [[user_message, None]]
@@ -36,13 +54,54 @@ def create_UI(llm_chain):
             history[-1][1] = ""
             history[-1][1] += bot_message
             return history
+        
+        def change_textbox(choice):
+            if choice == "wikipedia only":
+                template = wikipedia_template
+                llm_chain = create_chain_from_template(
+                    template, 
+                    rag, 
+                    llm_model
+                    )
+                return llm_chain
+            elif choice == "any website":
+                template = general_internet_template
+                llm_chain = create_chain_from_template(
+                    template, 
+                    rag, 
+                    llm_model
+                    )
+                return llm_chain
+            elif choice == "none":
+                return gr.Textbox(lines=8, visible=True, value="Lorem ipsum dolor sit amet"), gr.Button(interactive=True)
+            else:
+                return gr.Textbox(visible=False), gr.Button(interactive=False)
 
+        text = gr.Textbox(lines=2, interactive=True, show_copy_button=True)
+        radio.change(fn=change_textbox, inputs=radio, outputs=[text, submit_btn])
         msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(bot, chatbot, chatbot)
         clear.click(lambda: None, None, chatbot, queue=False)
     return demo
 
 
 if __name__ == "__main__":
+    template = wikipedia_template
+    prompt = PromptTemplate(template=template, input_variables=["query"])
+    
+
+    llm_model = ChatCohere(
+        cohere_api_key=COHERE_API_KEY,
+        )
+
+    rag = CohereRagRetriever(llm=llm_model,)
+
+
+    llm_chain = create_chain_from_template(
+        template, 
+        rag, 
+        llm_model
+        )
+
     demo = create_UI(llm_chain)
     demo.queue()
     # demo.launch()
